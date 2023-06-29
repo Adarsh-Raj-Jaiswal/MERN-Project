25cec76fc62853f67de5a28df52c5367e8fd6494
Adarsh Raj Jaiswal <adarshraj3309@gmail.com>
2023-06-29 18:13:50 +0530

    added routes and controllers for update and delete

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -5,7 +5,7 @@
 const express = require('express');
 
 // performing object destructuring assignment e.i. extracting specific properties from the obect and assign them to the variables with the same name
-const { getAllProducts, createProduct } = require('../controllers/productController'); // importing two functions
+const { getAllProducts, createProduct, updateProduct, deleteProduct } = require('../controllers/productController'); // importing  functions
 
 // creating a new router object
 const router = express.Router();
@@ -16,6 +16,11 @@ router.route('/Products') // defining routes for /Products path
 
 router.route('/Products/new') // defining routes for /Products/new path
     .post(createProduct); // defining a route for the POST HTTP method providing createProduct as callback function
+
+router.route('/Products/:id')
+    .put(updateProduct)
+    .delete(deleteProduct);
+
 
 // exporting the router object
 module.exports = router;
